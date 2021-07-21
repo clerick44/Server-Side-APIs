2bbb38beb9e96c82da52695e67ee19e8db1be974
clerick44 <clerick44@hotmail.com>
2021-07-21 13:08:57 -0500

    completed time based color changes and updates

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // get local time/date
-var today = moment().format("ddd, MMMM Do h:mma");
-var now = moment().format("H");
-console.log(now);
+var today = moment().format("ddd, MMMM Do h:mm:ss a");
+var now = moment().hour();
 //display local time in jumbotron
 $("#currentDay").html(today);
 
-// declare memos variable and retrieve/combine data from local storage
+// declare memos variable
 var memos = [
   { hour: "8", memo: "" },
   { hour: "9", memo: "" },
@@ -19,32 +18,63 @@ var memos = [
   { hour: "17", memo: "" },
 ];
 
-function retrieveMemos() {
-  var storedMemos = JSON.parse(localStorage.getItem("memos"));
-  if (storedMemos !== null) {
-    memos = storedMemos;
+$(document).ready(function () {
+  function refreshTime() {
+    setInterval(function () {
+      $("#currentDay").html(moment().format("ddd, MMMM Do h:mm:ss a"));
+      console.log("a");
+      timeCheck();
+    }, 1000);
   }
-  displayMemos();
-}
 
-retrieveMemos();
+  //retrieve/combine data from local storage
+  function retrieveMemos() {
+    var storedMemos = JSON.parse(localStorage.getItem("memos"));
+    if (storedMemos !== null) {
+      memos = storedMemos;
+    }
+    displayMemos();
+  }
 
-function displayMemos() {
-  $(".description").each(function (i) {
-    $(this).val(memos[i].memo);
+  function displayMemos() {
+    $(".description").each(function (i) {
+      $(this).val(memos[i].memo);
+    });
+  }
+
+  //compare current hour to hour in schedule and
+  // change color based on past, present, future
+  function timeCheck() {
+    $(".timeBlock").each(function (i) {
+      var time = parseInt($(this).attr("id"));
+      console.log(now);
+      console.log(time);
+      if (time < now) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      } else if (time === now) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  //listen for click on save icon
+  //take text from textarea and save to local file and display
+  $(".btn").on("click", function () {
+    var hour = $(this).parent().attr("id");
+    var memo = $(this).siblings(".description").val();
+    memos[hour - 8].memo = memo;
+    localStorage.setItem("memos", JSON.stringify(memos));
   });
-}
 
-//listen for click on save icon
-//take text from textarea and save to local file and display
-$(".btn").on("click", function () {
-  var hour = $(this).parent().attr("id");
-  var memo = $(this).siblings(".description").val();
-  memos[hour - 8].memo = memo;
-  localStorage.setItem("memos", JSON.stringify(memos));
-  displayMemos();
+  retrieveMemos();
+  //   timeCheck();
+  refreshTime();
 });
-
-//compare current hour to hour in schedule and
-// change color based on past, present, future
-$(".timeBlock").each(function () {});
